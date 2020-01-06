aec3a046911af6fd6dcbaf726b1d191fd8fb259b
Ahmed Khokar <amkhokar@gmail.com>
2020-01-06 12:54:34 -0800

    first example response

--- public/js/script.js
+++ public/js/script.js
@@ -20,27 +20,8 @@ $(document).ready(function() {
     });
   }
   getVotes().then(function(response) {
-    ALL_VOTES = JSON.parse(response).body.data;
-    ALL_VOTES.forEach(element => {
-      $("#result").append(`<li>
-        <img src=${element.imageUrl}><br>
-        <b>Name:</b><br>${element.name}<br>
-        <b>Type:</b><br>${element.type}<br>
-        </li><hr>`);
-    });
-  });
-
-  $("button").on("click", function(event) {
-    $("#result").empty();
-
-    ALL_VOTES.filter(ele => ele.type === event.target.value).forEach(
-      element => {
-        $("#result").append(`<li>
-        <img src=${element.imageUrl}><br>
-        <b>Name:</b><br>${element.name}<br>
-        <b>Type:</b><br>${element.type}<br>
-        </li><hr>`);
-      }
-    );
+    ALL_VOTES = JSON.parse(response);
+    console.log(ALL_VOTES);
+    $("#result").append(JSON.stringify(ALL_VOTES));
   });
 });
